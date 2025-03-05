67ca7139f2b89a6885ed7e55c172cd47db7c588d
MahadhevanThirumalai <mahadhevancse@gmail.com>
2025-03-05 11:07:03 +0530

    Band-name-Generator

---
+++ Day-1-task/Brand_Name_Generator.py
@@ -0,0 +1,7 @@
+#print("Click Run to run the final project you build.")
+
+print("Welcome to the Band Name Generator.")
+name1=input("what's the name of city you grew up in?\n")
+name2=input("what's your pet's name?\n")
+fullname=name1+ " " +name2
+print("Your band name could be " + fullname)
